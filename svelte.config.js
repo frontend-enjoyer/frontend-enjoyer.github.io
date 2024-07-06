2f70dada8b2d238aaf8dcef694e3604e06a9532f
m.gladyshev <m.nikolashina@innopolis.university>
2024-07-06 17:31:07 +0300

    fix pipeline

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,7 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		})
 	}
 };
